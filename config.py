0d8a0c5744b36b41e9cde95638d1d67d89b2c624
Rosemaryne <139486123+Rosemaryne@users.noreply.github.com>
2024-10-25 22:36:34 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,47 @@
+TOKEN =''
+SCHEDULE_PATH = {
+    "monday": {
+        "1": "e k - A337, Grupp 2, Küppar",
+        "2": "ühisk - A536, Kim",
+        "3": "matemaatika - A234, Perminova/ A440, Ionova/ A542, Pinaeva",
+        "4": "e k - A337, Kim",
+        "5": "i k - Grupp 1, A438, Art/ Grupp 2, A339, Sepp",
+        "6": "muus - A205, Trubatsova",
+        "8": "e k - Grupp 1, A434, Küppar/ kkp - Grupp 2, saal, Kusnerov"
+    },
+    "tuesday": {
+        "2": "ühisk - A434, Kim",
+        "3": "matemaatika - A234, Perminova/ A440, Ionova/ A542, Pinaeva",
+        "4": "i k - Grupp 1, A346, Art/ Grupp 2, A339, Sepp",
+        "5": "keem - Grupp 1, A236, Bitova/ Grupp 2, A237, Ojavere",
+        "6": "kirj - A434, Trussova",
+        "7": "ajal - A337, Kim",
+        "8": "ajal - A337, Kim"
+    },
+    "wednesday": {
+        "1": "bio - A541, Anna P",
+        "2": "füüs - A235, Krikmann",
+        "3": "ajal - A337, Kim",
+        "4": "ei ole tundi",
+        "5": "i k - Grupp 1, A242, Art/ Grupp 2, A339, Sepp",
+        "6": "v k - A433, Abisogomjan"
+    },
+    "thursday": {
+        "1": "matemaatika - A234, Perminova/ A235, Pinaeva",
+        "2": "v k - A433, Abisogomjan",
+        "3": "kkp - Grupp 1, saal, Kusnerov/ e k - Grupp 2, A337, Küppar",
+        "4": "inim - A541, Anna P",
+        "5": "bio - A541, Anna P",
+        "6": "kkp - Grupp 2, saal, Kusnerov"
+    },
+    "friday": {
+        "1": "e k - Grupp 1, A337, Küppar",
+        "2": "geo - A436, Ennok",
+        "3": "ekirj - A337, Küppar",
+        "4": "matemaatika - A234, Perminova/ A440, Ionova/ A433, Pinaeva",
+        "5": "kkp - Grupp 1, saal, Kusnerov",
+    },
+    
+    
+
+}
